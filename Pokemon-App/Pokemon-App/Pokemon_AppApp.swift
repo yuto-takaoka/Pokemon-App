a0d25e20bc32822751d97300fc3cb3eb21b3b925
yuto-takaoka <163364922+yuto-takaoka@users.noreply.github.com>
2024-03-15 14:55:17 +0900

    Add files via upload

---
+++ Pokemon-App/Pokemon-App/Pokemon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_AppApp.swift
+//  Pokemon-App
+//
+//  Created by 高岡悠斗 on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
